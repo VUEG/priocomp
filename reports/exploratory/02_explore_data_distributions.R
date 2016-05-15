6e613aea7c398a56a80abbe60d077ea3f1419636
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-05-15 13:33:23 +0200

    Update images

--- reports/exploratory/02_explore_data_distributions.R
+++ reports/exploratory/02_explore_data_distributions.R
@@ -9,21 +9,20 @@ options(scipen = 9999)
 # in the RStudio project root "priocomp" or Knitr folder "reports".
 
 if (basename(getwd()) == "priocomp") {
-  data_path <- "data/interim/"
+  data_path <- "data/"
 } else {
-  data_path <- "../../data/interim/"
+  data_path <- "../../data/"
 }
 
-harmonized_files <- list.files(file.path(data_path, "harmonized"),
+harmonized_files <- list.files(file.path(data_path, "interim/harmonized"),
                                pattern = ".\\.tif$", recursive = TRUE,
                                full.names = TRUE)
-rescaled_files <- list.files(file.path(data_path, "rescaled"),
+processed_files <- list.files(file.path(data_path, "processed"),
                              pattern = ".\\.tif$", recursive = TRUE,
                              full.names = TRUE)
 
-# Create rasterstacks
-harmonized_rasters <- raster::stack(harmonized_files)
-rescaled_rasters <- raster::stack(rescaled_files)
+# Remove data_coverage.tif
+processed_files <- processed_files[2:length(processed_files)]
 
 # Histograms
 
@@ -33,23 +32,23 @@ plots1 <- lapply(harmonized_files, function(x) {
                  main = names(x))
 })
 
-arr_plots1 <- do.call(grid.arrange, plots)
-ggsave("reports/figures/harmonized_rasters.png", arr_plots1, width = 16, height = 16)
+arr_plots1 <- do.call(grid.arrange, plots1)
+ggsave("reports/figures/harmonized_rasters_histogram.png", arr_plots1, width = 16, height = 16)
 
-plots2 <- lapply(rescaled_files, function(x) {
+plots2 <- lapply(processed_files, function(x) {
   x <- raster(x)
   ggplot2::qplot(getValues(x), geom = "histogram", bins = 50, xlab = "Value",
                  main = names(x))
 })
 
 arr_plots2 <- do.call(grid.arrange, plots2)
-ggsave("reports/figures/rescaled_rasters.png", arr_plots2, width = 16, height = 16)
+ggsave("reports/figures/processed_rasters_histogram.png", arr_plots2, width = 16, height = 16)
 
 # Boxplots
 
-png(filename = "reports/figures/rescaled_rasters_boxplot.png", width = 1200,
+png(filename = "reports/figures/processed_rasters_boxplot.png", width = 1200,
     height = 1000)
 par(mar = c(3, 18, 3, 3))
-boxplot(rescaled_rasters, maxpixels = 1000000, horizontal = TRUE, las = 2,
-        main = "Rescaled (normalized) values", ylab = "Value")
+boxplot(raster::stack(processed_files), maxpixels = 1000000, horizontal = TRUE, las = 2,
+        main = "Processed (harmonized + normalized) values", ylab = "Value")
 dev.off()
