974ff6e76181aa0115aa7d1f34b15911fb4b8cf8
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-06-07 11:21:45 +0200

    Add parse_data_manifest()

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,37 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import os
+import yaml
+
+
+def parse_data_manifest(infile):
+    """ Parse datasets from a data manifest file.
+
+    :param infile String path to input YAML data manifest file.
+    :return a dictionary of files with provider name as key and downloadable
+            URIs as values
+    """
+    provider_datasets = {}
+    if not os.path.exists(infile):
+        raise IOError("Input data manifest file does not exist")
+
+    data_manifest = yaml.safe_load(open(infile, 'r'))
+    for item in data_manifest:
+        # Item should only have one key, which is the URI
+        uri = list(item.keys())[0]
+        # Loop over the collections
+        for provider_collection in item[uri]:
+            provider = provider_collection['provider']
+            provider_datasets[provider] = []
+            collection = provider_collection['collections']
+            # Loop over collections and datasets
+            for collection_name, datasets in collection.items():
+                for dataset in datasets:
+                    dataset_uri = "/".join([uri, provider, collection_name,
+                                           dataset])
+                    provider_datasets[provider].append(dataset_uri)
+
+    return provider_datasets
 
 
 def pick_from_list(items, suffix):
