bdaf7378f7c996a91ad7423e11a8134f7801da7c
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-06-08 22:10:17 +0200

    Convert bat-files to sh-scripts

---
+++ analyses/zonation/priocomp/01_caz.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+zig4 -r 01_caz/01_caz.dat 01_caz/01_caz.spp 01_caz/01_caz_out/01_caz.txt 0 0 1 0 --grid-output-formats=compressed-tif --image-output-formats=png
---
+++ analyses/zonation/priocomp/03_caz_wgt.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+zig4 -r 03_caz_wgt/03_caz_wgt.dat 03_caz_wgt/03_caz_wgt.spp 03_caz_wgt/03_caz_wgt_out/03_caz_wgt.txt 0 0 1 0 --grid-output-formats=compressed-tif --image-output-formats=png
