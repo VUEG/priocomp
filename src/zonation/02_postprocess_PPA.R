372a567eb9a5089e634154774f7f0bebbb6e1405
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-06-10 08:56:41 +0200

    Update PPA file path

--- src/zonation/02_postprocess_PPA.R
+++ src/zonation/02_postprocess_PPA.R
@@ -4,7 +4,7 @@ library(zonator)
 source("src/utils.R")
 
 priocomp_root <- "analyses/zonation/priocomp/"
-variants <- c("01_caz", "02_abf")
-ppa_shp <- "data/processed/nuts/NUTS_RG_01M_2013/level2/NUTS_RG_01M_2013_level2_subset.shp"
+variants <- c("01_caz", "03_abf_wgt")
+ppa_shp <- "data/processed/eurostat/nuts/level2/NUTS_RG_01M_2013_level2.shp"
 
 postprocess_ppa(priocomp_root, variants, ppa_shp)
