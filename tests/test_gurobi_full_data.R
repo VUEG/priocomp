b64c02fcbd66c06981911db81127a46742d43237
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-06-13 13:28:21 +0200

    Modify tests

--- tests/test_gurobi_full_data.R
+++ tests/test_gurobi_full_data.R
@@ -1,14 +1,22 @@
+#!/usr/bin/env r
+
+library(magrittr)
 library(protectr)
 library(raster)
 library(rasterVis)
 library(viridis)
 
-data_dir <- "data/processed/features/ol_normalized/provide/"
+data_dir <- "../data/processed/features_ol_normalized/provide/"
+if (!file.exists(data_dir)) {
+  stop("Dir ", data_dir, " does not exist")
+}
+message("Reading in data...")
 raster_files <- list.files(path = data_dir, pattern = ".+\\.tif$",
                            full.names = TRUE, recursive = TRUE)
 
-es_rasters <- raster::stack(raster_files)
+es_rasters <- raster::stack(raster_files[1:5])
 
+message("Creating cost layer...")
 cost <- extent(es_rasters) %>%
   raster(nrows = nrow(es_rasters), ncols = ncol(es_rasters), vals = 1)
 
@@ -19,9 +27,10 @@ cost <- extent(es_rasters) %>%
 # Fill areas normalle NA (NoData) with cost of 0
 cost[is.na(es_rasters[[1]])] <- 0
 
-levelplot(cost, main = "Cost", margin = FALSE, scales = list(draw = FALSE),
-          col.regions = viridis)
+#levelplot(cost, main = "Cost", margin = FALSE, scales = list(draw = FALSE),
+#          col.regions = viridis)
 
+message("Converting NoData to zeros...")
 # NAs (NoData) must be raplaced wiht 0s for GUROBI
 es_rasters_filled <- es_rasters
 es_rasters_filled[is.na(es_rasters_filled)] <- 0
@@ -31,7 +40,9 @@ es_rasters_filled <- raster::stack(es_rasters_filled)
 budgets <- c(0.05)
 results_mc <- list()
 for (b in budgets) {
+  message("Optimizing with target budget ", b)
   b_cells <- b * raster::cellStats(cost, "sum")
   results <- protectr::gurobi_maxcoverage(cost, es_rasters_filled, budget = b_cells)
   results_mc[[as.character(b)]] <- results
 }
+save(results_mc, "results_mc.RData")
