2dc89c9493e9f1297afae51f89bb7b54e65e20bb
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-06-15 11:58:51 +0200

    Update gurobi test script

--- tests/test_gurobi_full_data.R
+++ tests/test_gurobi_full_data.R
@@ -6,39 +6,41 @@ library(raster)
 #library(rasterVis)
 #library(viridis)
 
-data_dir <- "../data/processed/features_ol_normalized/provide/"
+# Set raster options
+rasterOptions(tmpdir = "/data/tmp/raster",
+              progress = "text",
+              chunksize = 10000000,
+              maxmemory = 200000000)
+
+data_dir <- "data/processed/features_ol_normalized"
 if (!file.exists(data_dir)) {
   stop("Dir ", data_dir, " does not exist")
 }
+
 message("Reading in data...")
 raster_files <- list.files(path = data_dir, pattern = ".+\\.tif$",
                            full.names = TRUE, recursive = TRUE)
+message("Found ", length(raster_files), " rasters")
+
 if (length(raster_files) == 0) {
   stop("No input files found in ", data_dir)
 }
 
-es_rasters <- raster::stack(raster_files[1:5])
+rasters <- raster::stack(raster_files)
 
 message("Creating cost layer...")
-cost <- extent(es_rasters) %>%
-  raster(nrows = nrow(es_rasters), ncols = ncol(es_rasters), vals = 1)
-
-# Alternatively, cost data can be simulated
-# cost <- gaussian_field(r, 20, mean = 1000, variance = 500) #%>%
-#  setNames("cost")
+cost <- extent(rasters) %>%
+  raster(nrows = nrow(rasters), ncols = ncol(rasters), vals = 1)
 
 # Fill areas normalle NA (NoData) with cost of 0
-cost[is.na(es_rasters[[1]])] <- 0
-
-#levelplot(cost, main = "Cost", margin = FALSE, scales = list(draw = FALSE),
-#          col.regions = viridis)
+cost[is.na(rasters[[1]])] <- 0
 
 message("Converting NoData to zeros...")
 # NAs (NoData) must be raplaced wiht 0s for GUROBI
 #browser()
-es_rasters_filled <- es_rasters
-es_rasters_filled[is.na(es_rasters_filled)] <- 0
-es_rasters_filled <- raster::stack(es_rasters_filled)
+rasters_filled <- rasters
+rasters_filled[is.na(rasters_filled)] <- 0
+rasters_filled <- raster::stack(rasters_filled)
 
 # Solve the maximum coverage problem for a range of target budgets
 budgets <- c(0.05, 0.10)
@@ -46,8 +48,42 @@ results_mc <- list()
 for (b in budgets) {
   message("Optimizing with target budget ", b)
   b_cells <- b * raster::cellStats(cost, "sum")
-  results <- protectr::gurobi_maxcoverage(cost, es_rasters_filled, budget = b_cells)
+  results <- protectr::gurobi_maxcoverage(cost, rasters_filled, budget = b_cells)
   results_mc[[as.character(b)]] <- results
   message("Saving results...")
-  save(results_mc, file="results_mc.RData")
+  save(results_mc, file = "results_mc.RData")
 }
+
+
+# Process results ---------------------------------------------------------
+
+# process_gurobi_results <- function(results, template_raster) {
+#   result_raster <- template_raster
+#   result_raster[] <- results$x
+#   # Fill in the nodata
+#   result_raster[is.na(template_raster)] <- NA
+#   # Make this a categorical raster
+#   result_raster <- raster::ratify(result_raster)
+#   rat <- levels(result_raster)[[1]]
+#   # In some cases (most notably when budget is 100%) all cells might be
+#   # selected. Check for this.
+#   if (nrow(rat) == 2) {
+#     rat$status <- c("Not Selected", "Selected")
+#   } else {
+#     rat$status <- c("Selected")
+#   }
+#   levels(result_raster) <- rat
+#   return(result_raster)
+# }
+#
+# load("tests/results_mc.RData")
+#
+# nrows <- nrow(es_rasters)
+# ncols <- ncol(es_rasters)
+#
+# template <- extent(es_rasters) %>%
+#   raster(nrows = nrows, ncols = ncols, vals = 1)
+# template[is.na(es_rasters[[1]])] <- NA
+#
+# milp_mc <- raster::stack(lapply(results_mc, process_gurobi_results, template))
+# names(milp_mc) <- paste0("percent", budgets*100)
