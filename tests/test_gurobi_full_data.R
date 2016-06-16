5ab24814ec489b5cb137a10206e9ed10aa488af5
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-06-16 14:19:09 +0200

    Update gurobi test

--- tests/test_gurobi_full_data.R
+++ tests/test_gurobi_full_data.R
@@ -9,10 +9,10 @@ library(raster)
 # Set raster options
 rasterOptions(tmpdir = "/data/tmp/raster",
               progress = "text",
-              chunksize = 10000000,
+              chunksize = 100000000,
               maxmemory = 200000000)
 
-data_dir <- "data/processed/features_ol_normalized"
+data_dir <- "../data/processed/features_ol_normalized"
 if (!file.exists(data_dir)) {
   stop("Dir ", data_dir, " does not exist")
 }
@@ -32,15 +32,15 @@ message("Creating cost layer...")
 cost <- extent(rasters) %>%
   raster(nrows = nrow(rasters), ncols = ncol(rasters), vals = 1)
 
-# Fill areas normally NA (NoData) with cost of 0
-cost[is.na(rasters[[1]])] <- 0
+# Fill areas marked 0 with 0 in the cost data as well
+cost[rasters[[1]] == 0] <- 0
 
-message("Converting NoData to zeros...")
+#message("Converting NoData to zeros...")
 # NAs (NoData) must be raplaced wiht 0s for GUROBI
 #browser()
-rasters_filled <- rasters
-rasters_filled[is.na(rasters_filled)] <- 0
-rasters_filled <- raster::stack(rasters_filled)
+#rasters_filled <- rasters
+#rasters_filled[is.na(rasters_filled)] <- 0
+#rasters_filled <- raster::stack(rasters_filled)
 
 # Solve the maximum coverage problem for a range of target budgets
 budgets <- c(0.05, 0.10)
@@ -48,7 +48,7 @@ results_mc <- list()
 for (b in budgets) {
   message("Optimizing with target budget ", b)
   b_cells <- b * raster::cellStats(cost, "sum")
-  results <- protectr::gurobi_maxcoverage(cost, rasters_filled, budget = b_cells)
+  results <- protectr::gurobi_maxcoverage(cost, rasters, budget = b_cells)
   results_mc[[as.character(b)]] <- results
   message("Saving results...")
   save(results_mc, file = "results_mc.RData")
