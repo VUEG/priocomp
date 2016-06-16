c634e9576abf647b8692b06fddddc38a53675448
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-06-16 15:35:27 +0200

    Add code for processing gurobi results

--- tests/test_gurobi_full_data.R
+++ tests/test_gurobi_full_data.R
@@ -53,37 +53,3 @@ for (b in budgets) {
   message("Saving results...")
   save(results_mc, file = "results_mc.RData")
 }
-
-
-# Process results ---------------------------------------------------------
-
-# process_gurobi_results <- function(results, template_raster) {
-#   result_raster <- template_raster
-#   result_raster[] <- results$x
-#   # Fill in the nodata
-#   result_raster[is.na(template_raster)] <- NA
-#   # Make this a categorical raster
-#   result_raster <- raster::ratify(result_raster)
-#   rat <- levels(result_raster)[[1]]
-#   # In some cases (most notably when budget is 100%) all cells might be
-#   # selected. Check for this.
-#   if (nrow(rat) == 2) {
-#     rat$status <- c("Not Selected", "Selected")
-#   } else {
-#     rat$status <- c("Selected")
-#   }
-#   levels(result_raster) <- rat
-#   return(result_raster)
-# }
-#
-# load("tests/results_mc.RData")
-#
-# nrows <- nrow(es_rasters)
-# ncols <- ncol(es_rasters)
-#
-# template <- extent(es_rasters) %>%
-#   raster(nrows = nrows, ncols = ncols, vals = 1)
-# template[is.na(es_rasters[[1]])] <- NA
-#
-# milp_mc <- raster::stack(lapply(results_mc, process_gurobi_results, template))
-# names(milp_mc) <- paste0("percent", budgets*100)
