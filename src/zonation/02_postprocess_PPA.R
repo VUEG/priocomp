4debb4d067510052dbd3345313dc900eca0a2c62
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-06-17 08:56:15 +0200

    PPA post-process 05 and 06

--- src/zonation/02_postprocess_PPA.R
+++ src/zonation/02_postprocess_PPA.R
@@ -4,7 +4,8 @@ library(zonator)
 source("src/utils.R")
 
 priocomp_root <- "analyses/zonation/priocomp/"
-variants <- c("01_caz", "02_abf", "03_caz_wgt", "04_abf_wgt")
+variants <- c("01_caz", "02_abf", "03_caz_wgt", "04_abf_wgt", "05_caz_es",
+              "06_abf_es")
 ppa_shp <- "data/processed/eurostat/nuts/level2/NUTS_RG_01M_2013_level2.shp"
 
 postprocess_ppa(priocomp_root, variants, ppa_shp)
