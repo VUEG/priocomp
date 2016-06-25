7c7eba4e4b06270fb0dbb14acfc9092768994d5f
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-06-25 16:54:22 +0200

    Test gurobi with half the data

--- tests/test_gurobi_full_data.R
+++ tests/test_gurobi_full_data.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env r
 
 library(magrittr)
-library(protectr)
+library(prioritizr)
 library(raster)
 #library(rasterVis)
 #library(viridis)
@@ -12,6 +12,9 @@ rasterOptions(tmpdir = "/data/tmp/raster",
               chunksize = 100000000,
               maxmemory = 200000000)
 
+# Start load timer
+load_start <- Sys.time()
+
 data_dir <- "../data/processed/features_ol_normalized"
 if (!file.exists(data_dir)) {
   stop("Dir ", data_dir, " does not exist")
@@ -19,7 +22,7 @@ if (!file.exists(data_dir)) {
 
 message("Reading in data...")
 raster_files <- list.files(path = data_dir, pattern = ".+\\.tif$",
-                           full.names = TRUE, recursive = TRUE)[1:12]
+                           full.names = TRUE, recursive = TRUE)[1:386]
 message("Found ", length(raster_files), " rasters")
 for (raster_file in raster_files) {
   message(raster_file)
@@ -44,16 +47,29 @@ cost[rasters[[1]] == 0] <- 0
 #rasters_filled[is.na(rasters_filled)] <- 0
 #rasters_filled <- raster::stack(rasters_filled)
 
+# Stop load timer
+load_time <- Sys.time() - load_start
+# Start solver timer
+solver_start <- Sys.time()
+
 # Solve the maximum coverage problem for a range of target budgets
 budgets <- seq(0.05, 1, 0.05)
 results_mc <- list()
 for (b in budgets) {
   message("Optimizing with target budget ", b)
   b_cells <- b * raster::cellStats(cost, "sum")
-  mc_model <- prioritizr::maxcover_model(x = cost, features = sp_rasters_filled,
+  mc_model <- prioritizr::maxcover_model(x = cost, features = rasters,
                                          budget = b_cells)
-  mc_results <- prioritize(mc_model, gap = 0.001)
+  mc_results <- prioritizr::prioritize(mc_model, gap = 0.001)
   results_mc[[as.character(b)]] <- mc_results
   message("Saving results...")
   save(results_mc, file = "results_mc.RData")
 }
+solver_time <- Sys.time() - solver_start
+
+message("Results for ", length(raster_files), " rasters")
+message("Load time: ")
+load_time
+message("Solver time: ")
+solver_time
+
