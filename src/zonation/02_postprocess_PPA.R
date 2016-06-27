6a93684bf8645f56ebad8808c08e358776fb06f2
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-06-27 13:32:39 +0200

    Add 07 and 08 to PPA post-processing

--- src/zonation/02_postprocess_PPA.R
+++ src/zonation/02_postprocess_PPA.R
@@ -5,7 +5,7 @@ source("src/utils.R")
 
 priocomp_root <- "analyses/zonation/priocomp/"
 variants <- c("01_caz", "02_abf", "03_caz_wgt", "04_abf_wgt", "05_caz_es",
-              "06_abf_es")
+              "06_abf_es", "07_caz_bd", "08_abf_bd")
 ppa_shp <- "data/processed/eurostat/nuts/level2/NUTS_RG_01M_2013_level2.shp"
 
 postprocess_ppa(priocomp_root, variants, ppa_shp)
