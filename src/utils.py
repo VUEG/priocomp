f3a7fe53cfcedefdf275c6e490556cafd2982e95
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-09-07 18:40:23 +0200

    New data manifest structure
    Both in the yaml file as well as in utils.

--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,102 @@
 """ Utility module for the workflow."""
 import logging
 import os
+import pdb
 import yaml
 from colorlog import ColoredFormatter
+
+# Classes ---------------------------------------------------------------------
+
+
+class DataManifest(object):
+
+    """ Class representing a data manifest file.
+
+    Methods of this class can be used to access the content of the data
+    manifest file in various ways.
+    """
+
+    def __init__(self, infile):
+        if os.path.exists(infile):
+            self._infile = infile
+        else:
+            raise IOError("Input data manifest file does not exist")
+
+        # Read and parse the data manifest file
+        self._data = self.parse_data_manifest()
+
+    def parse_data_manifest(self):
+        """ Parse datasets from a data manifest file.
+
+        Current implementation can work with the following hierarchy:
+            [URI]: dict
+                "provider": str
+                "collections": list
+                    [NAME]: dict
+                        "category": str
+                        "metadata": list
+                        "resources": list
+        """
+
+        def parse_collection(collection, collection_name):
+            collection_data = {}
+            collection_item = collection[collection_name]
+            # Get the values for checking
+            collection_keys = list(collection_item.keys())
+            # Category is optional
+            if 'category' in collection_keys:
+                collection_data['collection_category'] = collection_item['category']
+            # Metadata is optional
+            if 'metadata' in collection_keys:
+                collection_data['collection_metadata'] = collection_item['metadata']
+            # Resources is required
+            if 'resources' in collection_keys:
+                collection_data['collection_resorces'] = collection_item['resources']
+            else:
+                raise ValueError("Collection {} contains no resources".format(collection_name))
+
+            return collection_data
+
+        items = []
+
+        data_manifest = yaml.safe_load(open(self._infile, 'r'))
+        for item in data_manifest:
+            # Item should only have one key, which is the URI
+            uri = list(item.keys())[0]
+            # Loop over the provider content. A single provider can have
+            # multiple collections.
+            for provider_content in item[uri]:
+                # Get the textual name of the provider
+                provider_name = provider_content['provider']
+                # Loop over the collections for this provider
+                for collection in provider_content['collections']:
+                    # Collection name is the only key
+                    collection_name = list(collection.keys())[0]
+                    # Check whether collection item is a dict or a list
+                    # (indicating subgroups)
+                    if isinstance(collection[collection_name], dict):
+                        collection_data = parse_collection(collection,
+                                                           collection_name)
+                        collection_data['uri'] = uri
+                        collection_data['provider_name'] = provider_name
+                        collection_data['collection_name'] = collection_name
+                        items.append(collection_data)
+                    elif isinstance(collection[collection_name], list):
+                        for subcollection in collection[collection_name]:
+                            subcollection_name = list(subcollection.keys())[0]
+                            subcollection_data = parse_collection(subcollection,
+                                                                  subcollection_name)
+                            subcollection_data['uri'] = uri
+                            subcollection_data['provider_name'] = provider_name
+                            subcollection_data['collection_name'] = collection_name
+                            subcollection_data['subcollection_name'] = subcollection_name
+                            items.append(subcollection_data)
+                    else:
+                        raise ValueError("Invalid collection type")
+
+        return items
+
+# Functions -------------------------------------------------------------------
 
 
 def get_iteration_prefix(i, total):
@@ -47,57 +141,6 @@ def get_local_logger(name, log_file=None, debug=False):
         llogger.addHandler(fileHandler)
 
     return llogger
-
-
-def parse_data_manifest(infile):
-    """ Parse datasets from a data manifest file.
-
-    Current implementation can work with the following hierarchy:
-        uri:
-            provider: str
-            collections: list | dict
-
-    If "collections" value is a list, then there are 3 hierarchy levels. If
-    it is a dict, then there are 4 hierarchy levels. The key of the dict is a
-    subcollection name and value is a list of datasets.
-
-    :param infile String path to input YAML data manifest file.
-    :return a dictionary of files with provider name as key and downloadable
-            URIs as values
-    """
-    provider_datasets = {}
-    if not os.path.exists(infile):
-        raise IOError("Input data manifest file does not exist")
-
-    data_manifest = yaml.safe_load(open(infile, 'r'))
-    for item in data_manifest:
-        # Item should only have one key, which is the URIq
-        uri = list(item.keys())[0]
-        # Loop over the collections
-        for provider_collection in item[uri]:
-            provider = provider_collection['provider']
-            provider_datasets[provider] = []
-            collection = provider_collection['collections']
-            # Loop over collections and datasets
-            for collection_name, datasets in collection.items():
-                # 3 levels of hierarchy
-                if isinstance(datasets, list):
-                    for dataset in datasets:
-                        dataset_uri = "/".join([uri, provider, collection_name,
-                                               dataset])
-                        provider_datasets[provider].append(dataset_uri)
-                elif isinstance(datasets, dict):
-                    for subcollection_name, subdatasets in datasets.items():
-                        for subdataset in subdatasets:
-                            dataset_uri = "/".join([uri, provider,
-                                                    collection_name,
-                                                    subcollection_name,
-                                                    subdataset])
-                            provider_datasets[provider].append(dataset_uri)
-                else:
-                    raise TypeError("Unknown collections type")
-
-    return provider_datasets
 
 
 def pick_from_list(items, suffix):
