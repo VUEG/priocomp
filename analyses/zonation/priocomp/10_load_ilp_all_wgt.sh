e569debeacf733bd200ffee6ed9b90d36592258f
Ubuntu <Joona Lehtomki>
2016-09-19 09:58:19 +0200

    Fix 10 sh

--- analyses/zonation/priocomp/10_load_ilp_all_wgt.sh
+++ analyses/zonation/priocomp/10_load_ilp_all_wgt.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-zig4 -r -l../../ILP/ilp_eu26_all_weights.tif 10_load_ilp_all_wgt/10_load_ilp_all_wgt.dat 10_load_ilp_all_wgt/10_load_ilp_all_wgt.spp 10_load_ilp_all_wgt/10_load_ilp_all_wgt_out/10_load_ilp_all_wgt.txt 0.0 0 1.0 0 --grid-output-formats=compressed-tif --image-output-formats=png
+zig4 -l../../ILP/ilp_eu26_all_weights.tif 10_load_ilp_all_wgt/10_load_ilp_all_wgt.dat 10_load_ilp_all_wgt/10_load_ilp_all_wgt.spp 10_load_ilp_all_wgt/10_load_ilp_all_wgt_out/10_load_ilp_all_wgt.txt 0.0 0 1.0 0 --grid-output-formats=compressed-tif --image-output-formats=png
