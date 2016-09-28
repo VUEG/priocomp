2601afc044340fb2194b1ab8acbab24489998c7a
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-09-28 15:55:11 +0200

    Add variant 13_load_abf_wgt_rwr_all
    Load cell removal order from rwr_all_weights, but get data from 04.

---
+++ analyses/zonation/priocomp/13_load_abf_wgt_rwr_all.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+zig4 -l../../rwr_eu26_all_weights.tif 13_load_abf_wgt_rwr_all/13_load_abf_wgt_rwr_all.dat 13_load_abf_wgt_rwr_all/13_load_abf_wgt_rwr_all.spp 13_load_abf_wgt_rwr_all/13_load_abf_wgt_rwr_all_out/13_load_abf_wgt_rwr_all.txt 0.0 0 1.0 0 --grid-output-formats=compressed-tif --image-output-formats=png
