cf8c3973fd28611d28e6713b281bf8d7dd79bfc4
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-09-28 15:41:55 +0200

    Add variant 11_load_abf_bd_es
    Load cell removal order from 08, but get data from 06.

---
+++ analyses/zonation/priocomp/12_load_abf_bd_es.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+zig4 -l08_abf_bd/08_abf_bd_out/08_abf_bd.rank_expanded.compressed.tif 12_load_abf_bd_es/12_load_abf_bd_es.dat 12_load_abf_bd_es/12_load_abf_bd_es.spp 12_load_abf_bd_es/12_load_abf_bd_es_out/12_load_abf_bd_es.txt 0.0 0 1.0 0 --grid-output-formats=compressed-tif --image-output-formats=png
