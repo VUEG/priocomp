298b3fa4a81e0491f5367681bfc8812e196e1826
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-09-30 15:38:13 +0200

    Update Fig 4

--- src/04_visualization/05_figure_04.R
+++ src/04_visualization/05_figure_04.R
@@ -3,25 +3,23 @@ library(sp)
 library(tmap)
 library(viridis)
 
-
 # Load data ---------------------------------------------------------------
 
 nuts2_var_ds <- "analyses/comparison/nuts2_rank_variation.shp"
 nuts2_var <- maptools::readShapePoly(nuts2_var_ds,
-                                     proj4string = CRS("+init=epsg:3035"))
-
+                                   proj4string = CRS("+init=epsg:3035"))
 
 # Plot data ---------------------------------------------------------------
 
 nuts2_var_maps <- tm_shape(nuts2_var) +
-  tm_polygons(c("agg_mean", "agg_std"),
+  tm_polygons(c("agg_mean", "agg_std"), title = c("", ""),
               palette = viridis::viridis(5),
               legend.is.portrait = TRUE) +
-  tm_format_Europe(title = c("Mean rank", "Standard deviation"),
-                   inner.margins = c(0.02, 0.02, 0.02, 0),
-                   legend.show = TRUE) +
+  tm_format_Europe(title = c("Mean priority rank", "Std or prriority ranks"),
+                   inner.margins = c(0.02, 0.02, 0.02, -0.05),
+                   legend.show = TRUE, legend.position = c("left", "top"),
+                   legend.bg.color = "white") +
   tm_style_grey()
-
 
 # Save map ----------------------------------------------------------------
 
