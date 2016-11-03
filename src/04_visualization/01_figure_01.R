0225f53c57a36c502c38acea950b08d9430c2d3a
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-11-03 09:23:57 +0100

    Update Fig 1

--- src/04_visualization/01_figure_01.R
+++ src/04_visualization/01_figure_01.R
@@ -5,24 +5,31 @@ library(sp)
 library(tmap)
 library(viridis)
 
+data(Europe)
+
 # Define CRS (ETRS89 / ETRS-LAEA), http://spatialreference.org/ref/epsg/3035/
 crs_etrslaea <- CRS("+init=epsg:3035")
 # Read in area-of-interest, a selceted subset of NUTS level 0 regions
-shp_ds <- "data/processed/eurostat/nuts_level0/NUTS_RG_01M_2013_level0.shp"
+europe_ds <- "data/processed/eurostat/nuts_level0/NUTS_RG_01M_2013_level0.shp"
+aoi_ds <- "data/processed/eurostat/nuts_level0/NUTS_RG_01M_2013_level0.shp"
 aoi <- maptools::readShapePoly(shp_ds, proj4string = crs_etrslaea)
 
 # Remove Liechtenstein
-aoi <- subset(aoi, aoi$nuts_id != "LI")
+aoi <- subset(aoi, aoi$NUTS_ID != "LI")
 
 # Define color palette using viridis
 col_pal <- viridis::viridis(3)
 
-tm <- tm_shape(aoi) +
-        tm_fill(col_pal[2]) +
-        tm_text(text = "nuts_id", size = 0.6, col = "black", shadow = FALSE) +
-        tm_borders("#202021", lwd = 0.3) +
-        tm_format_Europe(title = "EU-25",
-                         inner.margins = c(0, 0.05, 0.02, 0)) +
-        tm_style_grey()
+tm_eur <- tm_shape(Europe) +
+  tm_fill("lightgrey") +
+  tm_format_Europe(inner.margins = c(0, 0.05, 0.02, 0))
+
+tm_aoi <- tm_shape(aoi, is.master = TRUE) +
+  tm_fill(col_pal[2]) +
+  tm_text(text = "NUTS_ID", size = 0.6, col = "black", shadow = FALSE) +
+  tm_borders("lightgrey", lwd = 0.3) +
+  tm_format_Europe(inner.margins = c(0, 0.05, 0.02, 0))
+
+tm <- tm_eur + tm_aoi + tm_scale_bar(position = c("left", "top"))
 
 save_tmap(tm, "reports/figures/01_figure_01.png", width = 1200, height = 1400)
