d437c0a6abffe980655da0d3d2e0f38e3cdd6647
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-11-07 13:35:35 +0100

    Fix 13 and 14 sh-scripts

--- analyses/zonation/priocomp/13_load_abf_wgt_rwr_all.sh
+++ analyses/zonation/priocomp/13_load_abf_wgt_rwr_all.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-zig4 -l../../RWR/rwr_eu26_all_weights_expanded.tif 13_load_abf_wgt_rwr_all/13_load_abf_wgt_rwr_all.dat 13_load_abf_wgt_rwr_all/13_load_abf_wgt_rwr_all.spp 13_load_abf_wgt_rwr_all/13_load_abf_wgt_rwr_all_out/13_load_abf_wgt_rwr_all.txt 0.0 0 1.0 0 --grid-output-formats=compressed-tif --image-output-formats=png
+zig4 -l../../RWR/rwr_all_weights_expanded.tif 13_load_abf_wgt_rwr_all/13_load_abf_wgt_rwr_all.dat 13_load_abf_wgt_rwr_all/13_load_abf_wgt_rwr_all.spp 13_load_abf_wgt_rwr_all/13_load_abf_wgt_rwr_all_out/13_load_abf_wgt_rwr_all.txt 0.0 0 1.0 0 --grid-output-formats=compressed-tif --image-output-formats=png
