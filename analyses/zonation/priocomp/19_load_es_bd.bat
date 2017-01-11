9253d8c7d6dae4b3b1a2d86d6287404e7cfa84d1
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2017-01-11 13:20:23 +0100

    Fix sh and bat files for 19 and 20
    Manually (!)

--- analyses/zonation/priocomp/19_load_es_bd.bat
+++ analyses/zonation/priocomp/19_load_es_bd.bat
@@ -1,1 +1,1 @@
-call zig4.exe -l12_abf_bd/12_abf_bd_out/08_abf_es.rank_bd_matched.compressed.tif 19_load_es_bd/19_load_es_bd.dat 19_load_es_bd/19_load_es_bd.spp 19_load_es_bd/19_load_es_bd_out/19_load_es_bd.txt 0 0 1 0 --grid-output-formats=compressed-tif --image-output-formats=png
+call zig4.exe -l08_abf_es/08_abf_es/08_abf_es.rank_bd_matched.compressed.tif 19_load_es_bd/19_load_es_bd.dat 19_load_es_bd/19_load_es_bd.spp 19_load_es_bd/19_load_es_bd_out/19_load_es_bd.txt 0 0 1 0 --grid-output-formats=compressed-tif --image-output-formats=png
--- analyses/zonation/priocomp/19_load_es_bd.sh
+++ analyses/zonation/priocomp/19_load_es_bd.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-zig4 -l12_abf_bd/12_abf_bd_out/08_abf_es.rank_bd_matched.compressed.tif 19_load_es_bd/19_load_es_bd.dat 19_load_es_bd/19_load_es_bd.spp 19_load_es_bd/19_load_es_bd_out/19_load_es_bd.txt 0 0 1 0 --grid-output-formats=compressed-tif --image-output-formats=png
+zig4 -l08_abf_es/08_abf_es/08_abf_es.rank_bd_matched.compressed.tif 19_load_es_bd/19_load_es_bd.dat 19_load_es_bd/19_load_es_bd.spp 19_load_es_bd/19_load_es_bd_out/19_load_es_bd.txt 0 0 1 0 --grid-output-formats=compressed-tif --image-output-formats=png
