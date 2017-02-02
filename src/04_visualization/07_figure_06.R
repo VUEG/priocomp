7acc48ced0d8f5297970326d6021baf78624ecee
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2017-02-02 17:31:50 +0100

    Update Fig6

--- src/04_visualization/07_figure_06.R
+++ src/04_visualization/07_figure_06.R
@@ -3,14 +3,135 @@ library(ggplot2)
 library(ggthemes)
 library(gridExtra)
 library(tidyr)
+library(raster)
 library(zonator)
 library(viridis)
 
 # Helper functions --------------------------------------------------------
 
+create_cost_plot <- function(x, title = NULL) {
+
+  x_lab <- "\nFraction of the landscape"
+  x_scale <- scale_x_continuous(breaks = seq(0, 1, 0.2),
+                                labels = paste(100 * seq(1, 0, -0.2), "%"))
+  y_scale <- scale_y_continuous(breaks = seq(0, 1, 0.2))
+  # Define x-axis vlines that are used to link to Fig 5
+  vlines_x <- c(0.75, 0.9, 0.98)
+  vlines_labs <- c("25%", "10%", "2%")
+  vlines_labs_x <- vlines_x + 0.02
+  vlines_labs_y <- 1.03
+
+  p1 <- ggplot2::ggplot(x, aes(x = pr_lost, y = cost, color = variant)) +
+    geom_vline(xintercept = vlines_x, alpha = 0.5, linetype = 3) +
+    annotate("text", x = vlines_labs_x, y = vlines_labs_y,
+             label = vlines_labs, size = 3) +
+    geom_line(size = 1) + x_scale + y_scale + xlab(x_lab) + ylab("") +
+    scale_color_manual("", values =  rev(viridis(3, end = 0.9))) +
+    ylab("Relateive cost of the solution\n") + ggtitle(title) +
+    theme_minimal() +
+    theme(legend.position = c(0.20, 0.35),
+          legend.justification = c(0.5, 0),
+          legend.key.width = unit(1,"cm"))
+  return(p1)
+}
+
+create_perf_plot <- function(x, title = NULL) {
+
+  x_lab <- "\nFraction of the landscape"
+  x_scale <- scale_x_continuous(breaks = seq(0, 1, 0.2),
+                                labels = paste(100 * seq(1, 0, -0.2), "%"))
+  y_scale <- scale_y_continuous(breaks = seq(0, 1, 0.2),
+                                labels = paste(100 * seq(0, 1, 0.2), "%"))
+  # Define x-axis vlines that are used to link to Fig 5
+  vlines_x <- c(0.75, 0.9, 0.98)
+  vlines_labs <- c("25%", "10%", "2%")
+  vlines_labs_x <- vlines_x + 0.02
+  vlines_labs_y <- 1.03
+
+  p1 <- ggplot2::ggplot(x, aes(x = pr_lost, y = ave_pr, color = variant,
+                               linetype = variant)) +
+    geom_vline(xintercept = vlines_x, alpha = 0.5, linetype = 3) +
+    annotate("text", x = vlines_labs_x, y = vlines_labs_y,
+             label = vlines_labs, size = 3) +
+    geom_line(size = 1) + x_scale + y_scale + xlab(x_lab) + ylab("") +
+    scale_linetype_manual("", values = rep(1:3, 3)) +
+    scale_color_manual("", values =  rev(rep(viridis(3, end = 0.9), 1, each = 3))) +
+    ylab("Average feature distribution covered\n") + ggtitle(title) +
+    theme_minimal() +
+    theme(legend.position = c(0.20, 0.05),
+          legend.justification = c(0.5, 0),
+          legend.key.width = unit(1,"cm"))
+  return(p1)
+}
+
+get_costs <- function(cost_raster, rank_raster, n = 1002,
+                      rescale_cost = TRUE) {
+  cost_raster_src <- raster::getValues(cost_raster)
+  rank_raster_src <- raster::getValues(rank_raster)
+
+  # Rank raster will always have <= informative cells
+  mask <- is.na(rank_raster_src)
+  # Get only non-NoData data
+  cost_raster_src <- cost_raster_src[!mask]
+  rank_raster_src <- rank_raster_src[!mask]
+
+  # Make matrix
+  cost_matrix <- cbind(rank_raster_src, cost_raster_src)
+  # Order by rank
+  cost_matrix <- cost_matrix[sort.list(cost_matrix[,1], decreasing = TRUE), ]
+  # Instead of proportion remaining, use proportion lost
+  cost_matrix[,1] <- 1.0 - cost_matrix[,1]
+  # Some cost data can be NA, replace with 0
+  cost_matrix[is.na(cost_matrix[,2]),2] <- 0
+  # Calculate cumulative sum
+  cost_matrix <- cbind(cost_matrix, cumsum(cost_matrix[,2]))
+  # Select every nth row
+  every <- nrow(cost_matrix) / n
+  cost_matrix <- cost_matrix[seq(1, nrow(cost_matrix), every), ]
+  # Coerce into a dataframe
+  cost_df <- as.data.frame(cost_matrix)
+  names(cost_df) <- c("pr_lost", "cost_per_cell", "cost")
+
+  if (rescale_cost) {
+    cost_df$cost <- cost_df$cost / max(cost_df$cost)
+  }
+
+  return(cost_df)
+}
+
+get_grp_means <- function(variant, groups) {
+  zonator::groupnames(variant) <- groups
+  grp_crvs <- zonator::curves(variant, groups = TRUE)
+
+  # Use the first part of the first group label as a method label
+  method_label <- unlist(strsplit(groups, "_"))[1]
+  # Generate current group labels
+  group_labels <- paste0("mean.", groups[1:2])
+
+  # Get group means, must use SE with dplyr.
+  # NOTE: mean for ALL needs to calculated from ES + BD because CST is
+  # included in the non-grouped curves
+  lng_grp_1 <- group_labels[1]
+  lng_grp_2 <- group_labels[2]
+  sht_grp_1 <- groups[[1]]
+  sht_grp_2 <- groups[[2]]
+  sht_grp_3 <- paste0(method_label, "_ALL")
+  mutate_call <- lazyeval::interp(~ (a + b) / 2,
+                                  a = as.name(sht_grp_1),
+                                  b = as.name(sht_grp_2))
+  grp_means <- grp_crvs %>%
+    dplyr::select_("pr_lost", lng_grp_1, lng_grp_2) %>%
+    dplyr::rename_(.dots = setNames(list(lng_grp_1, lng_grp_2),
+                                    c(sht_grp_1, sht_grp_2))) %>%
+    dplyr::mutate_(.dots = setNames(list(mutate_call), sht_grp_3)) %>%
+    tidyr::gather_("variant", "ave_pr", c(sht_grp_1, sht_grp_2, sht_grp_3))
+
+  return(grp_means)
+}
+
 get_perf_level <- function(data, x) {
   perf_data <- data %>%
-    dplyr::group_by(method, group, feature) %>%
+    dplyr::group_by(variant, group, feature) %>%
     dplyr::filter(pr_lost >= x) %>%
     dplyr::arrange(pr_lost) %>%
     dplyr::slice(1) %>%
@@ -18,143 +139,91 @@ get_perf_level <- function(data, x) {
   return(perf_data)
 }
 
-# Load variants and get curves data ---------------------------------------
+# Load variants -----------------------------------------------------------
 
 zproject <- zonator::load_zproject('analyses/zonation/priocomp/')
+
+zon_groups <- c("1" = "ZON_ES", "2" = "ZON_BD", "3" = "ZON_CST")
+rwr_groups <- c("1" = "RWR_ES", "2" = "RWR_BD", "3" = "RWR_CST")
+ilp_groups <- c("1" = "ILP_ES", "2" = "ILP_BD", "3" = "ILP_CST")
+
 v04_abf_all_wgt <- zonator::get_variant(zproject, 4)
+v06_abf_all_wgt_cst <- zonator::get_variant(zproject, 6)
 
-v04_crvs <- zonator::curves(v04_abf_all_wgt, groups = FALSE)
-v13_crvs <- zonator::read_curves("analyses/zonation/priocomp/13_load_abf_wgt_rwr_all/13_load_abf_wgt_rwr_all_out/13_load_abf_wgt_rwr_all.curves.txt")
-v14_crvs <- zonator::read_curves("analyses/zonation/priocomp/14_load_abf_wgt_ilp_all/14_load_abf_wgt_ilp_all_out/14_load_abf_wgt_ilp_all.curves.txt")
+v23_load_rwr_all <- zonator::get_variant(zproject, 23)
+v25_load_rwr_all_cst <- zonator::get_variant(zproject, 25)
 
-# Mean curves data --------------------------------------------------------
+v24_load_ilp_all <- zonator::get_variant(zproject, 24)
+v26_load_ilp_all_cst <- zonator::get_variant(zproject, 26)
 
-v04_mean <- v04_crvs %>%
-  dplyr::select(pr_lost, ave_pr, w_pr) %>%
-  dplyr::mutate(variant = "ZON_ALL")
+# Mean group curves data --------------------------------------------------
 
-# FIXME! For now (2016-11-08), calculate means manually. Groups file
-# definition were not correct in the last run.
-v04_es_mean <- v04_crvs %>%
-  dplyr::select(woodprod_average:species_richness_vascular_plants) %>%
-  dplyr::mutate(ave_pr = rowMeans(., na.rm = TRUE)) %>%
-  dplyr::select(ave_pr) %>%
-  dplyr::bind_cols(dplyr::select(v04_crvs, pr_lost), .) %>%
-  dplyr::mutate(variant = "ZON_ES")
+v04_grp_mean <- get_grp_means(v04_abf_all_wgt, zon_groups)
+v06_grp_mean <- get_grp_means(v06_abf_all_wgt_cst, zon_groups)
 
-v04_bd_mean <- v04_crvs %>%
-  dplyr::select(-(pr_lost:species_richness_vascular_plants)) %>%
-  dplyr::mutate(ave_pr = rowMeans(., na.rm = TRUE)) %>%
-  dplyr::select(ave_pr) %>%
-  dplyr::bind_cols(dplyr::select(v04_crvs, pr_lost), .) %>%
-  dplyr::mutate(variant = "ZON_BD")
+v23_grp_mean <- get_grp_means(v23_load_rwr_all, rwr_groups)
+v25_grp_mean <- get_grp_means(v25_load_rwr_all_cst, rwr_groups)
 
-v04_grp_mean <- dplyr::bind_rows(v04_es_mean, v04_bd_mean)
+v24_grp_mean <- get_grp_means(v24_load_ilp_all, ilp_groups)
+v26_grp_mean <- get_grp_means(v26_load_ilp_all_cst, ilp_groups)
 
-v13_crvs <- zonator::read_curves("analyses/zonation/priocomp/13_load_abf_wgt_rwr_all/13_load_abf_wgt_rwr_all_out/13_load_abf_wgt_rwr_all.curves.txt")
-v13_mean <- v13_crvs %>%
-  dplyr::select(pr_lost, ave_pr, w_pr) %>%
-  dplyr::mutate(variant = "RWR_ALL")
+# Combine performance data from different methods, for no-costs and costs
+method_perf_nocost <- dplyr::bind_rows(v04_grp_mean, v23_grp_mean,
+                                       v24_grp_mean)
+method_perf_cost <- dplyr::bind_rows(v06_grp_mean, v25_grp_mean,
+                                     v26_grp_mean)
 
-# FIXME! For now (2016-11-08), calculate means manually. Groups file
-# definition were not correct in the last run.
-v13_es_mean <- v13_crvs %>%
-  dplyr::select(f1:f9) %>%
-  dplyr::mutate(ave_pr = rowMeans(., na.rm = TRUE)) %>%
-  dplyr::select(ave_pr) %>%
-  dplyr::bind_cols(dplyr::select(v13_crvs, pr_lost), .) %>%
-  dplyr::mutate(variant = "RWR_ES")
+# Cost data ---------------------------------------------------------------
 
-v13_bd_mean <- v13_crvs %>%
-  dplyr::select(-(pr_lost:f9)) %>%
-  dplyr::mutate(ave_pr = rowMeans(., na.rm = TRUE)) %>%
-  dplyr::select(ave_pr) %>%
-  dplyr::bind_cols(dplyr::select(v13_crvs, pr_lost), .) %>%
-  dplyr::mutate(variant = "RWR_BD")
+# Read in the cost data rasters
+cost_raster <- raster::raster("data/processed/features/eea/pop_density/pop_density_v5.tif")
+v06_rank_raster <- zonator::rank_raster(v06_abf_all_wgt_cst)
+v25_rank_raster <- zonator::rank_raster(v25_load_rwr_all_cst)
+v26_rank_raster <- zonator::rank_raster(v26_load_ilp_all_cst)
 
-v13_grp_mean <- dplyr::bind_rows(v13_es_mean, v13_bd_mean)
+v06_cost <- get_costs(cost_raster = cost_raster,
+                      rank_raster = v06_rank_raster)
+v06_cost$variant <- "ZON_ALL"
+v25_cost <- get_costs(cost_raster = cost_raster,
+                      rank_raster = v25_rank_raster)
+v25_cost$variant <- "RWR_ALL"
+v26_cost <- get_costs(cost_raster = cost_raster,
+                      rank_raster = v26_rank_raster)
+v26_cost$variant <- "ILP_ALL"
 
-v14_crvs <- zonator::read_curves("analyses/zonation/priocomp/14_load_abf_wgt_ilp_all/14_load_abf_wgt_ilp_all_out/14_load_abf_wgt_ilp_all.curves.txt")
-v14_mean <- v14_crvs %>%
-  dplyr::select(pr_lost, ave_pr, w_pr) %>%
-  dplyr::mutate(variant = "ILP_ALL")
+cost_perf_cost <- dplyr::bind_rows(v06_cost, v25_cost, v26_cost)
 
-# FIXME! For now (2016-11-08), calculate means manually. Groups file
-# definition were not correct in the last run.
-v14_es_mean <- v14_crvs %>%
-  dplyr::select(f1:f9) %>%
-  dplyr::mutate(ave_pr = rowMeans(., na.rm = TRUE)) %>%
-  dplyr::select(ave_pr) %>%
-  dplyr::bind_cols(dplyr::select(v14_crvs, pr_lost), .) %>%
-  dplyr::mutate(variant = "ILP_ES")
+# Plot data ---------------------------------------------------------------
 
-v14_bd_mean <- v14_crvs %>%
-  dplyr::select(-(pr_lost:f9)) %>%
-  dplyr::mutate(ave_pr = rowMeans(., na.rm = TRUE)) %>%
-  dplyr::select(ave_pr) %>%
-  dplyr::bind_cols(dplyr::select(v14_crvs, pr_lost), .) %>%
-  dplyr::mutate(variant = "ILP_BD")
+p1 <- create_perf_plot(method_perf_nocost, title = "A")
+p2 <- create_perf_plot(method_perf_cost, title = "A")
 
-v14_grp_mean <- dplyr::bind_rows(v14_es_mean, v14_bd_mean)
-
-# Between methods
-method_perf <- dplyr::bind_rows(v04_mean, v04_grp_mean, v13_mean,
-                                v13_grp_mean, v14_mean, v14_grp_mean)
-
-x_lab <- "\nFraction of the landscape"
-x_scale <- scale_x_continuous(breaks = seq(0, 1, 0.2),
-                              labels = paste(100 * seq(1, 0, -0.2), "%"))
-y_scale <- scale_y_continuous(breaks = seq(0, 1, 0.2),
-                              labels = paste(100 * seq(0, 1, 0.2), "%"))
-# Define x-axis vlines that are used to link to Fig 5
-vlines_x <- c(0.75, 0.9, 0.98)
-vlines_labs <- c("25%", "10%", "2%")
-vlines_labs_x <- vlines_x + 0.03
-vlines_labs_y <- 1.0
-
-p1 <- ggplot2::ggplot(method_perf, aes(x = pr_lost, y = ave_pr,
-                                       color = variant,
-                                       linetype = variant)) +
-  geom_vline(xintercept = vlines_x, alpha = 0.5, linetype = 3) +
-  annotate("text", x = vlines_labs_x, y = vlines_labs_y,
-           label = vlines_labs, size = 3) +
-  geom_line(size = 1) + x_scale + y_scale + xlab(x_lab) + ylab("") +
-  scale_linetype_manual("", values = rep(1:3, 3)) +
-  scale_color_manual("", values =  rev(rep(viridis(3, end = 0.9), 1, each = 3))) +
-  ylab("Average feature distribution covered\n") + ggtitle("A") + theme_minimal() +
-  theme(legend.position = c(0.15, 0.05),
-        legend.justification = c(0.5, 0),
-        legend.key.width = unit(1,"cm"))
+p3 <- create_cost_plot(cost_perf_cost, title = "B")
 
 # All curves data ---------------------------------------------------------
 
-v04_crvs$method <- "ZON"
-v13_crvs$method <- "RWR"
-v14_crvs$method <- "ILP"
+v06_crvs <- zonator::curves(v06_abf_all_wgt_cst)
+v25_crvs <- zonator::curves(v25_load_rwr_all_cst)
+v26_crvs <- zonator::curves(v26_load_ilp_all_cst)
 
-v04_mean <- v04_crvs %>%
-  dplyr::select(pr_lost, ave_pr, w_pr) %>%
-  dplyr::mutate(variant = "ZON_ALL")
-
-# zonator::read_curves() doesn't produce feature names, but they are the same
-# as in 04.
-names(v13_crvs) <- names(v04_crvs)
-names(v14_crvs) <- names(v04_crvs)
+v06_crvs$variant <- "ZON"
+v25_crvs$variant <- "RWR"
+v26_crvs$variant <- "ILP"
 
 # ES features
-all_crvs_es <- dplyr::bind_rows(v04_crvs, v13_crvs, v14_crvs) %>%
+all_crvs_es <- dplyr::bind_rows(v06_crvs, v25_crvs, v26_crvs) %>%
   dplyr::select(-(cost:ext2)) %>%
-  dplyr::select(pr_lost:species_richness_vascular_plants, method) %>%
-  tidyr::gather(feature, pr_rem, -pr_lost, -method) %>%
+  dplyr::select(pr_lost:species_richness_vascular_plants, variant) %>%
+  tidyr::gather(feature, pr_rem, -pr_lost, -variant) %>%
   dplyr::mutate(group = "ES") %>%
-  dplyr::select(pr_lost, method, group, feature, pr_rem)
+  dplyr::select(pr_lost, variant, group, feature, pr_rem)
 
 # BD features
-all_crvs_bd <- dplyr::bind_rows(v04_crvs, v13_crvs, v14_crvs) %>%
+all_crvs_bd <- dplyr::bind_rows(v06_crvs, v25_crvs, v26_crvs) %>%
   dplyr::select(-(cost:species_richness_vascular_plants)) %>%
-  tidyr::gather(feature, pr_rem, -pr_lost, -method) %>%
+  tidyr::gather(feature, pr_rem, -pr_lost, -variant) %>%
   dplyr::mutate(group = "BD") %>%
-  dplyr::select(pr_lost, method, group, feature, pr_rem)
+  dplyr::select(pr_lost, variant, group, feature, pr_rem)
 
 # ALL is effectively ES + BD
 all_crvs <- dplyr::bind_rows(all_crvs_es, all_crvs_bd)
@@ -170,18 +239,18 @@ perf_details$level <- factor(perf_details$level,
                              levels = perf_levels,
                              labels = paste0(100 - perf_levels * 100, "%"))
 
-p2 <- ggplot(perf_details, aes(x = factor(group), y = pr_rem,
-                                 fill = factor(method))) +
+p4 <- ggplot(perf_details, aes(x = factor(group), y = pr_rem,
+                                 fill = factor(variant))) +
   scale_fill_manual("Method", values =  rev(rep(viridis(3, end = 0.9), 3, each = 1))) +
   geom_boxplot(outlier.colour = "darkgrey", outlier.size = 0.2) +
   facet_wrap(~level) +
   ylab("Feature distribution covered\n") + xlab("\nData group") +
   scale_y_continuous(breaks = seq(0, 1, 0.25), labels = paste0(seq(0, 100, 25), "%")) +
-  ggtitle("B") + theme_minimal()
+  ggtitle("C") + theme_minimal()
 
 # Combine panels
-fig5 <- gridExtra::grid.arrange(p1, p2, ncol = 2, nrow = 1)
+fig5 <- gridExtra::grid.arrange(p2, p3, p4, ncol = 2, nrow = 2)
 
 # Save Figure -------------------------------------------------------------
 
-ggsave("reports/figures/15_figure_06.png", fig5, width = 12, height = 6)
+ggsave("reports/figures/figure06/01_figure_06.png", fig5, width = 9, height = 9)
