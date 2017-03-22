93a1ba2d3f0ae9ee154ac63803df463c2071d0e9
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2017-03-22 10:05:23 +0100

    Create an alterantive version of Fig 5

---
+++ src/04_visualization/07_figure_05_alt_landscape.R
@@ -0,0 +1,64 @@
+library(dplyr)
+library(ggplot2)
+library(ggthemes)
+library(tidyr)
+library(zonator)
+library(viridis)
+
+source("~/Dropbox/project.J-PriPA/japan-zsetup/R/00_lib/utils.R")
+
+# Load variants and configure groups --------------------------------------
+
+zproject <- zonator::load_zproject('analyses/zonation/priocomp/')
+
+highlights <- c(0.1, 0.25)
+
+# Separate ES and BD from ALL
+v04_nocosts <- get_stat_curves(zproject, variant_ids = 4, groups = TRUE) %>%
+  dplyr::mutate(variant = gsub("_1", "_es", variant)) %>%
+  dplyr::mutate(variant = gsub("_2", "_bd", variant)) %>%
+  dplyr::mutate(variant = factor(variant, levels = c("04_abf_all_wgt_es",
+                                                     "04_abf_all_wgt_bd"),
+                                 ordered = TRUE))
+
+v06_costs <- get_stat_curves(zproject, variant_ids = 6, groups = TRUE) %>%
+  dplyr::mutate(variant = gsub("_1", "_es", variant)) %>%
+  dplyr::mutate(variant = gsub("_2", "_bd", variant)) %>%
+  dplyr::mutate(variant = factor(variant, levels = c("06_abf_all_wgt_cst_es",
+                                                     "06_abf_all_wgt_cst_bd"),
+                                 ordered = TRUE))
+
+# No costs
+all_nocosts <- get_stat_curves(zproject, variant_ids = c(8, 12, 19, 20)) %>%
+  dplyr::bind_rows(v04_nocosts, .) %>%
+  dplyr::mutate(variant = factor(variant,
+                                 levels = c("08_abf_es", "04_abf_all_wgt_es", "20_load_bd_es",
+                                            "12_abf_bd", "04_abf_all_wgt_bd", "19_load_es_bd"),
+                                 ordered = TRUE))
+# Costs
+all_costs <- get_stat_curves(zproject, variant_ids = c(10, 14, 21, 22)) %>%
+  dplyr::bind_rows(v06_costs, .) %>%
+  dplyr::mutate(variant = factor(variant,
+                                 levels = c("10_abf_es_cst", "06_abf_all_wgt_cst_es", "22_load_bd_es_cst",
+                                            "14_abf_bd_cst", "06_abf_all_wgt_cst_bd", "21_load_es_bd_cst"),
+                                 ordered = TRUE))
+
+p1 <- all_nocosts %>%
+  plot_curves(title = "", non_param = TRUE, invert_x = TRUE,
+              nrow = 2, ncol = 3, highlights = highlights,
+              plot_min = TRUE, plot_max = TRUE,
+              labels = c("ES", "ES (rank ALL)", "ES (rank BD)",
+                         "BD", "BD (rank ALL)", "BD (rank ES)"))
+
+p2 <- all_costs %>%
+  plot_curves(title = "", non_param = TRUE, invert_x = TRUE,
+              nrow = 2, ncol = 3, highlights = highlights,
+              labels = c("ES", "ES (rank ALL)", "ES (rank BD)",
+                         "BD", "BD (rank ALL)", "BD (rank ES)"))
+
+# Save Figures ------------------------------------------------------------
+
+ggsave("reports/figures/figure05/01_figure_05_nocosts_alt_landscape.png", p1,
+       width = 9, height = 6)
+ggsave("reports/figures/figure05/02_figure_05_costs_alt_landscape.png", p2,
+       width = 9, height = 6)
