bb1d9bbdde4a04c51a75c9d434b64d748eb9e9c5
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2017-07-25 16:38:58 +0200

    Re-work Fig4

--- src/04_visualization/04_figure_04.R
+++ src/04_visualization/04_figure_04.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(ggplot2)
 library(ggthemes)
 library(gridExtra)
+library(hrbrthemes)
 library(tidyr)
 library(raster)
 library(zonator)
@@ -11,15 +12,66 @@ library(viridis)
 
 # Helper functions --------------------------------------------------------
 
-create_cost_plot <- function(x, title = NULL) {
+create_boxplot <- function(x, title = "", draw_legend = TRUE,
+                           draw_y_label = TRUE) {
+  # ES features
+  all_crvs_es <- x %>%
+    dplyr::select(-(cost:ext2)) %>%
+    dplyr::select(pr_lost:species_richness_vascular_plants, variant) %>%
+    tidyr::gather(feature, pr_rem, -pr_lost, -variant) %>%
+    dplyr::mutate(group = "ES") %>%
+    dplyr::select(pr_lost, variant, group, feature, pr_rem)
+
+  # BD features
+  all_crvs_bd <- x %>%
+    dplyr::select(-(cost:species_richness_vascular_plants)) %>%
+    tidyr::gather(feature, pr_rem, -pr_lost, -variant) %>%
+    dplyr::mutate(group = "BD") %>%
+    dplyr::select(pr_lost, variant, group, feature, pr_rem)
+
+  # ALL is effectively ES + BD
+  all_crvs <- dplyr::bind_rows(all_crvs_es, all_crvs_bd)
+  all_crvs_all <- all_crvs
+  all_crvs_all$group <- "ALL"
+  all_crvs <- dplyr::bind_rows(all_crvs, all_crvs_all)
+
+  perf_levels <- c(0.9, 0.98)
+  perf_details <- dplyr::bind_rows(lapply(perf_levels,
+                                          function(y) {get_perf_level(all_crvs, y)}))
+
+  perf_details$level <- factor(perf_details$level,
+                               levels = perf_levels,
+                               labels = paste0(100 - perf_levels * 100, "%"))
+
+  p <- ggplot(perf_details, aes(x = factor(group), y = pr_rem,
+                                 fill = factor(variant))) +
+    scale_fill_manual("Method", values =  rev(rep(viridis(3, end = 0.9), 3, each = 1))) +
+    geom_boxplot(outlier.colour = "darkgrey", outlier.size = 0.2, color = "darkgrey") +
+    facet_wrap(~level) +
+    xlab("\nData group") +
+    scale_y_continuous(breaks = seq(0, 1, 0.25), labels = paste0(seq(0, 100, 25), "%")) +
+    ggtitle(title) + theme_minimal()
+  if (draw_y_label) {
+    p <- p + ylab("Feature distribution covered\n")
+  } else {
+    p <- p + ylab("")
+  }
+  if (!draw_legend) {
+    p <- p + theme(legend.position = "none")
+  }
+  return(p)
+}
+
+create_cost_plot <- function(x, title = NULL, draw_legend = TRUE,
+                             draw_y_label = TRUE) {
 
   x_lab <- "\nFraction of the landscape"
   x_scale <- scale_x_continuous(breaks = seq(0, 1, 0.2),
                                 labels = paste(100 * seq(1, 0, -0.2), "%"))
   y_scale <- scale_y_continuous(breaks = seq(0, 1, 0.2))
   # Define x-axis vlines that are used to link to Fig 5
-  vlines_x <- c(0.75, 0.9, 0.98)
-  vlines_labs <- c("25%", "10%", "2%")
+  vlines_x <- c(0.9, 0.98)
+  vlines_labs <- c("10%", "2%")
   vlines_labs_x <- vlines_x + 0.02
   vlines_labs_y <- 1.03
 
@@ -29,15 +81,19 @@ create_cost_plot <- function(x, title = NULL) {
              label = vlines_labs, size = 3) +
     geom_line(size = 1) + x_scale + y_scale + xlab(x_lab) + ylab("") +
     scale_color_manual("", values =  rev(viridis(3, end = 0.9))) +
-    ylab("Relateive cost of the solution\n") + ggtitle(title) +
-    theme_minimal() +
-    theme(legend.position = c(0.20, 0.35),
-          legend.justification = c(0.5, 0),
-          legend.key.width = unit(1,"cm"))
+    ggtitle(title) + theme_minimal() +
+    theme(legend.key.width = unit(1,"cm"))
+  if (draw_y_label) {
+    p1 <- p1 + ylab("Relateive cost of the solution\n")
+  }
+  if (!draw_legend) {
+    p1 <- p1 + theme(legend.position = "none")
+  }
   return(p1)
 }
 
-create_perf_plot <- function(x, title = NULL) {
+create_perf_plot <- function(x, title = NULL, draw_legend = TRUE,
+                             draw_y_label = TRUE) {
 
   x_lab <- "\nFraction of the landscape"
   x_scale <- scale_x_continuous(breaks = seq(0, 1, 0.2),
@@ -45,8 +101,8 @@ create_perf_plot <- function(x, title = NULL) {
   y_scale <- scale_y_continuous(breaks = seq(0, 1, 0.2),
                                 labels = paste(100 * seq(0, 1, 0.2), "%"))
   # Define x-axis vlines that are used to link to Fig 5
-  vlines_x <- c(0.75, 0.9, 0.98)
-  vlines_labs <- c("25%", "10%", "2%")
+  vlines_x <- c(0.9, 0.98)
+  vlines_labs <- c("10%", "2%")
   vlines_labs_x <- vlines_x + 0.02
   vlines_labs_y <- 1.03
 
@@ -55,14 +111,19 @@ create_perf_plot <- function(x, title = NULL) {
     geom_vline(xintercept = vlines_x, alpha = 0.5, linetype = 3) +
     annotate("text", x = vlines_labs_x, y = vlines_labs_y,
              label = vlines_labs, size = 3) +
-    geom_line(size = 1) + x_scale + y_scale + xlab(x_lab) + ylab("") +
+    geom_line(size = 1) + x_scale + y_scale + xlab(x_lab) +
     scale_linetype_manual("", values = rep(1:3, 3)) +
     scale_color_manual("", values =  rev(rep(viridis(3, end = 0.9), 1, each = 3))) +
-    ylab("Average feature distribution covered\n") + ggtitle(title) +
-    theme_minimal() +
-    theme(legend.position = c(0.20, 0.05),
-          legend.justification = c(0.5, 0),
-          legend.key.width = unit(1,"cm"))
+    ggtitle(title) + theme_minimal() +
+    theme(legend.key.width = unit(1,"cm"))
+  if (draw_y_label) {
+    p1 <- p1 + ylab("Average feature distribution covered\n")
+  } else {
+    p1 <- p1 + ylab("")
+  }
+  if (!draw_legend) {
+    p1 <- p1 + theme(legend.position = "none")
+  }
   return(p1)
 }
 
@@ -140,6 +201,17 @@ get_perf_level <- function(data, x) {
   return(perf_data)
 }
 
+match_grob_width <- function(p1, p2) {
+  # Get the ggplot grobs
+  g1 <- ggplotGrob(p1)
+  g2 <- ggplotGrob(p2)
+  # Add a column to g1 and g2 (any width will do)
+  #g1 <- gtable_add_cols(g1, unit(1, "mm"))
+  # Set g5 width to be equal to g6 widths
+  g1$widths = g2$widths
+  return(g1)
+}
+
 # Load variants -----------------------------------------------------------
 
 zproject <- zonator::load_zproject('analyses/zonation/priocomp/')
@@ -178,13 +250,25 @@ method_perf_cost <- dplyr::bind_rows(v06_grp_mean, v25_grp_mean,
 
 # Read in the cost data rasters
 cost_raster <- raster::raster("data/processed/features/eea/pop_density/pop_density_v5.tif")
+v04_rank_raster <- zonator::rank_raster(v04_abf_all_wgt)
 v06_rank_raster <- zonator::rank_raster(v06_abf_all_wgt_cst)
+v23_rank_raster <- zonator::rank_raster(v23_load_rwr_all)
+v24_rank_raster <- zonator::rank_raster(v24_load_ilp_all)
 v25_rank_raster <- zonator::rank_raster(v25_load_rwr_all_cst)
 v26_rank_raster <- zonator::rank_raster(v26_load_ilp_all_cst)
 
+v04_cost <- get_costs(cost_raster = cost_raster,
+                      rank_raster = v04_rank_raster)
+v04_cost$variant <- "ZON_ALL"
 v06_cost <- get_costs(cost_raster = cost_raster,
                       rank_raster = v06_rank_raster)
 v06_cost$variant <- "ZON_ALL"
+v23_cost <- get_costs(cost_raster = cost_raster,
+                      rank_raster = v23_rank_raster)
+v23_cost$variant <- "RWR_ALL"
+v24_cost <- get_costs(cost_raster = cost_raster,
+                      rank_raster = v24_rank_raster)
+v24_cost$variant <- "ILP_ALL"
 v25_cost <- get_costs(cost_raster = cost_raster,
                       rank_raster = v25_rank_raster)
 v25_cost$variant <- "RWR_ALL"
@@ -192,69 +276,52 @@ v26_cost <- get_costs(cost_raster = cost_raster,
                       rank_raster = v26_rank_raster)
 v26_cost$variant <- "ILP_ALL"
 
+cost_perf_nocost <- dplyr::bind_rows(v04_cost, v23_cost, v24_cost)
 cost_perf_cost <- dplyr::bind_rows(v06_cost, v25_cost, v26_cost)
-
-# Plot data ---------------------------------------------------------------
-
-p1 <- create_perf_plot(method_perf_nocost, title = "A")
-p2 <- create_perf_plot(method_perf_cost, title = "A")
-
-p3 <- create_cost_plot(cost_perf_cost, title = "B")
 
 # All curves data ---------------------------------------------------------
 
-v06_crvs <- zonator::curves(v06_abf_all_wgt_cst)
-v25_crvs <- zonator::curves(v25_load_rwr_all_cst)
-v26_crvs <- zonator::curves(v26_load_ilp_all_cst)
+v04_crvs <- data.frame(zonator::curves(v04_abf_all_wgt))
+v23_crvs <- data.frame(zonator::curves(v23_load_rwr_all))
+v24_crvs <- data.frame(zonator::curves(v24_load_ilp_all))
+
+v04_crvs$variant <- "ZON"
+v23_crvs$variant <- "RWR"
+v24_crvs$variant <- "ILP"
+
+v06_crvs <- data.frame(zonator::curves(v06_abf_all_wgt_cst))
+v25_crvs <- data.frame(zonator::curves(v25_load_rwr_all_cst))
+v26_crvs <- data.frame(zonator::curves(v26_load_ilp_all_cst))
 
 v06_crvs$variant <- "ZON"
 v25_crvs$variant <- "RWR"
 v26_crvs$variant <- "ILP"
 
-# ES features
-all_crvs_es <- dplyr::bind_rows(v06_crvs, v25_crvs, v26_crvs) %>%
-  dplyr::select(-(cost:ext2)) %>%
-  dplyr::select(pr_lost:species_richness_vascular_plants, variant) %>%
-  tidyr::gather(feature, pr_rem, -pr_lost, -variant) %>%
-  dplyr::mutate(group = "ES") %>%
-  dplyr::select(pr_lost, variant, group, feature, pr_rem)
+all_dat_nocosts <- dplyr::bind_rows(v04_crvs, v23_crvs, v24_crvs)
+all_dat_costs <- dplyr::bind_rows(v06_crvs, v25_crvs, v26_crvs)
 
-# BD features
-all_crvs_bd <- dplyr::bind_rows(v06_crvs, v25_crvs, v26_crvs) %>%
-  dplyr::select(-(cost:species_richness_vascular_plants)) %>%
-  tidyr::gather(feature, pr_rem, -pr_lost, -variant) %>%
-  dplyr::mutate(group = "BD") %>%
-  dplyr::select(pr_lost, variant, group, feature, pr_rem)
+# Plot data ---------------------------------------------------------------
 
-# ALL is effectively ES + BD
-all_crvs <- dplyr::bind_rows(all_crvs_es, all_crvs_bd)
-all_crvs_all <- all_crvs
-all_crvs_all$group <- "ALL"
-all_crvs <- dplyr::bind_rows(all_crvs, all_crvs_all)
+p1 <- create_perf_plot(method_perf_nocost, title = "A", draw_legend = FALSE)
+p2 <- create_perf_plot(method_perf_cost, title = "B", draw_y_label = FALSE)
 
-perf_levels <- c(0.75, 0.9, 0.98)
-perf_details <- dplyr::bind_rows(lapply(perf_levels,
-                                        function(y) {get_perf_level(all_crvs, y)}))
+p3 <- create_cost_plot(cost_perf_nocost, title = "E", draw_legend = FALSE)
+p4 <- create_cost_plot(cost_perf_cost, title = "F", draw_y_label = FALSE)
 
-perf_details$level <- factor(perf_details$level,
-                             levels = perf_levels,
-                             labels = paste0(100 - perf_levels * 100, "%"))
+p5 <- create_boxplot(all_dat_nocosts, "C", draw_legend = FALSE)
+p6 <- create_boxplot(all_dat_costs, "D", draw_y_label = FALSE)
 
-p4 <- ggplot(perf_details, aes(x = factor(group), y = pr_rem,
-                                 fill = factor(variant))) +
-  scale_fill_manual("Method", values =  rev(rep(viridis(3, end = 0.9), 3, each = 1))) +
-  geom_boxplot(outlier.colour = "darkgrey", outlier.size = 0.2) +
-  facet_wrap(~level) +
-  ylab("Feature distribution covered\n") + xlab("\nData group") +
-  scale_y_continuous(breaks = seq(0, 1, 0.25), labels = paste0(seq(0, 100, 25), "%")) +
-  ggtitle("C") + theme_minimal()
+# Match subplot widths
+p1 <- match_grob_width(p1, p2)
+p3 <- match_grob_width(p3, p4)
+p5 <- match_grob_width(p5, p6)
 
 # Combine panels
-fig5 <- gridExtra::grid.arrange(p2, p3, p4, ncol = 2, nrow = 2)
+fig5 <- gridExtra::grid.arrange(p1, p2, p5, p6, p3, p4, ncol = 2, nrow = 3)
 
 # Save Figure -------------------------------------------------------------
 
-ggsave("reports/figures/figure04/01_figure_04.png", fig5, width = 9, height = 9)
+ggsave("reports/figures/figure04/01_figure_04.png", fig5, width = 12, height = 10)
 
 
 #  Extra: cost distribution -----------------------------------------------
